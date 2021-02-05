39f797e2b6d78f43cb747457b13b42aee5891cfb
Callum Rai <callum@rai.org.uk>
2021-02-05 01:44:53 +0000

    Initialised basic pair test

--- tests/test_pair.py
+++ tests/test_pair.py
@@ -1,9 +1,21 @@
 import unittest
+from radium.equity import Equity
+from radium.pair import Pair
 
 
 class TestPair(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        visa = Equity('V', '2015-01-01', '2021-01-01', 'A6O7S12U02K5YZO7')
+        mastercard = Equity('MA', '2015-01-01', '2021-01-01', 'A6O7S12U02K5YZO7')
+        v_ma = Pair(visa, mastercard)
+
+    def test_default(self):
+        """
+        Returns: Conducts useless test to ensure setUp runs checks for no exceptions thrown
+
+        """
+
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
