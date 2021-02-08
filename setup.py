e24f45da2d0757d658f7a1e82343b7a198d6f662
Callum Rai <callum@rai.org.uk>
2021-02-08 16:19:44 +0000

    Updated public ver

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 # This call to setup() does all the work
 setup(
     name="Radium-Tech",
-    version="0sim.0.1",
+    version="0.0.2",
     description="Intuitive backtesting for quantitative trading strategies.",
     long_description=README,
     long_description_content_type="text/markdown",
