256ae5b46b984e58af45921b34c0e22eb785d658
Ivan Erlic <mary.example@rypress.com>
2021-02-19 10:56:52 +0000

    Setup TestPairStrategy class

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -1,24 +1,29 @@
 import unittest
-from radium.equity import Equity
-from radium.pair import Pair
-from radium.strategy import BollingerPair
+
+from radium import Pair, Equity
+from radium.strategy import PairStrategy
 
 
-class TestBollingerPair(unittest.TestCase):
-    def setUp(self):
-        visa = Equity('V', '2015-01-01', '2021-01-01', API_KEY)
-        mastercard = Equity('MA', '2015-01-01', '2021-01-01', API_KEY)
-        v_ma = Pair(visa, mastercard)
-        v_ma_boll = BollingerPair(v_ma, 1, 0, 20)
-        v_ma_boll.plot()
+class TestPairStrategy(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with open('tests/api_key.txt') as file:
+            TestPairStrategy.API_KEY = file.readline()
 
-    def test_default(self):
+        TestPairStrategy.visa = Equity('V', '2015-01-01', '2021-01-01',
+                                        TestPairStrategy.API_KEY)
+        TestPairStrategy.mastercard = Equity('MA', '2015-01-01', '2021-01-01',
+                                              TestPairStrategy.API_KEY)
+        TestPairStrategy.V_MA = Pair(TestPairStrategy.visa,
+                                     TestPairStrategy.mastercard)
+        TestPairStrategy.strategy = PairStrategy(TestPairStrategy.V_MA)
+
+    def test_init_bad_input(self):
         """
-        Returns: Conducts useless test to ensure setUp runs checks for no exceptions thrown
-
+        Test exception handling of PairStrategy.__init__ method
         """
 
-        self.assertTrue(True)
+        self.assertRaises(TypeError, PairStrategy, 'not pair')
 
 
 if __name__ == '__main__':
