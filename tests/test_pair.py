ab7a6f8a6070b56d8403c864cb8c85712dbe20bf
Ivan Erlic <mary.example@rypress.com>
2021-02-20 10:33:30 +0000

    Edited setUpClass in test_pair and test_strategy

--- tests/test_pair.py
+++ tests/test_pair.py
@@ -11,13 +11,11 @@ class TestPair(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         with open('api_key.txt') as file:
-            TestPair.API_KEY = file.readline()
+            cls.API_KEY = file.readline()
 
-        TestPair.visa = \
-                Equity('V', '2015-01-01', '2021-01-01', TestPair.API_KEY)
-        TestPair.mastercard = \
-                Equity('MA', '2015-01-01', '2021-01-01', TestPair.API_KEY)
-        TestPair.V_MA = Pair(TestPair.visa, TestPair.mastercard)
+        cls.visa = Equity('V', '2015-01-01', '2021-01-01', cls.API_KEY)
+        cls.mastercard = Equity('MA', '2015-01-01', '2021-01-01', cls.API_KEY)
+        cls.V_MA = Pair(cls.visa, cls.mastercard)
 
     def test_init_bad_input(self):
         """ 
--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -7,16 +7,13 @@ from radium.strategy import PairStrategy
 class TestPairStrategy(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        with open('tests/api_key.txt') as file:
-            TestPairStrategy.API_KEY = file.readline()
+        with open('api_key.txt') as file:
+            cls.API_KEY = file.readline()
 
-        TestPairStrategy.visa = Equity('V', '2015-01-01', '2021-01-01',
-                                        TestPairStrategy.API_KEY)
-        TestPairStrategy.mastercard = Equity('MA', '2015-01-01', '2021-01-01',
-                                              TestPairStrategy.API_KEY)
-        TestPairStrategy.V_MA = Pair(TestPairStrategy.visa,
-                                     TestPairStrategy.mastercard)
-        TestPairStrategy.strategy = PairStrategy(TestPairStrategy.V_MA)
+        cls.visa = Equity('V', '2015-01-01', '2021-01-01', cls.API_KEY)
+        cls.mastercard = Equity('MA', '2015-01-01', '2021-01-01', cls.API_KEY)
+        cls.V_MA = Pair(cls.visa, cls.mastercard)
+        cls.strategy = PairStrategy(cls.V_MA)
 
     def test_init_bad_input(self):
         """
